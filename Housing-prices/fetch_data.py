560039cccf157ace2e80426d9ae8ea7f122317ec
Cecilia Laborde <cecilia@intellisense.io>
2019-05-27 09:05:08 +0200

    fetch data function

--- Housing-prices/fetch_data.py
+++ Housing-prices/fetch_data.py
@@ -1,3 +1,22 @@
 import os
 import tarfile
-from six.moves import urllib
+import urllib.request
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    "Dowload and extract tar file"
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+if __name__ == "__main__":
+    fetch_data()
