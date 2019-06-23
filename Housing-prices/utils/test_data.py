f659df4177f330bf5b1b5895f521b6d057421ccf
Cecilia Laborde <cecilia@intellisense.io>
2019-06-23 10:40:43 +0200

    refactoring

--- Housing-prices/utils/test_data.py
+++ Housing-prices/utils/test_data.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 from zlib import crc32
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 def split_train_test(data, test_ratio):
@@ -21,3 +23,17 @@ def split_train_test_by_id(data, test_ratio, id_column):
     ids = data[id_column]
     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
     return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+def stratified_shuffle(data):
+    data["income_cat"] = pd.cut(data["median_income"],
+                                bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                                labels=[1, 2, 3, 4, 5])
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(data, data["income_cat"]):
+        strat_train_set = data.loc[train_index]
+        strat_test_set = data.loc[test_index]
+
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
+    return strat_train_set, strat_test_set
