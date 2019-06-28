8b228475b8aa2e9e8dd06907ccdca8bbec9a7cc6
Cecilia Laborde <cecilialaborde@gmail.com>
2019-06-28 22:40:08 +0200

    cleaned up

--- Housing-prices/models/linear_regression.py
+++ Housing-prices/models/linear_regression.py
@@ -3,13 +3,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
-def linear_regression(data, prepared_data, labels, pipeline):
+def linear_regression(data_prepared, labels):
     lin_reg = LinearRegression()
     # we fit the model to processed data
-    lin_reg.fit(prepared_data, labels)
-
-    # we test the model on raw data
-    data_prepared = pipeline.fit_transform(data)
+    lin_reg.fit(data_prepared, labels)
     predictions = lin_reg.predict(data_prepared)
     mse = np.sqrt(mean_squared_error(labels, predictions))
     return predictions, mse
