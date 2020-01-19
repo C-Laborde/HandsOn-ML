b6572bee4634ac0c834febc9878afb3fbe94813e
Cecilia Laborde <cecilialaborde@gmail.com>
2020-01-19 16:58:51 +0200

    Model creation

--- NN/fashion_mnist.py
+++ NN/fashion_mnist.py
@@ -5,3 +5,13 @@ fashion_mnist = keras.datasets.fashion_mnist
 # validation set
 X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
 y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal",
+               "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+# Model with two hidden layers
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation="relu"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
